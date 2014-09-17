6fdd2559b5a92d0dc8bfac24af2e0a682471f5e8
Vibushanan <vibushanan1987@gmail.com>
2014-09-17 12:42:31 +0100

    for release r2

--- src/GitMain.java
+++ src/GitMain.java
@@ -7,6 +7,7 @@ public class GitMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("master v 1.0");
+      System.out.println("For R2 release");
 	}
 
 }
